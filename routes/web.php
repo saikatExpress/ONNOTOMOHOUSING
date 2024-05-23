692bcfba79bcff54987ebf25b8a8739f2aae89b6
Saikat Talukder <saikathosen444@gmail.com>
2024-05-23 13:24:04 +0600

    ONNOTOMO HOUSING LIMITED

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/link', function () {
+    Artisan::call('storage:link');
+    return 'Storage Link Successfully';
+});
+
+Route::get('/clear', function(){
+    Artisan::call('optimize:clear');
+    return 'Optimize Clear!.';
+})->name('clear');
+
+Route::get('/clear-cache', function() {
+    Artisan::call('config:cache');
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    return "Cache is cleared";
+})->name('clear.cache');
+
+Route::get('/', function () {
+    return view('welcome');
+});
